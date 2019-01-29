d2746b72a16c009573a2bf884d2bf0cd19355ea4
邓天骄 <jiao_snow@126.com>
2019-01-29 21:44:53 +0800

    初始化

---
+++ src/main/java/com/love/iLove/ILoveApplication.java
@@ -0,0 +1,14 @@
+package com.love.iLove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ILoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ILoveApplication.class, args);
+	}
+
+}
+
