5ed4122c52eb7ac113b28db115bf8335982fcb58
邓天骄 <jiao_snow@126.com>
2019-02-16 23:35:51 +0800

    创建helloword

--- src/main/java/com/love/iLove/ILoveApplication.java
+++ src/main/java/com/love/iLove/ILoveApplication.java
@@ -1,7 +1,13 @@
 package com.love.iLove;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ILoveApplication {
@@ -9,6 +15,37 @@ public class ILoveApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ILoveApplication.class, args);
 	}
+
+	/**
+	 * it's for set http url auto change to https
+	 */
+	@Bean
+	public TomcatServletWebServerFactory tomcatServletWebServerFactory(Connector connector){
+		TomcatServletWebServerFactory tomcat=new TomcatServletWebServerFactory(){
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint=new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection=new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+		tomcat.addAdditionalTomcatConnectors(connector);
+		return tomcat;
+	}
+
+	@Bean
+	public Connector httpConnector(){
+		Connector connector=new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		connector.setPort(8080);
+		connector.setSecure(false);
+		connector.setRedirectPort(8443);
+		return connector;
+	}
+
 
 }
 
