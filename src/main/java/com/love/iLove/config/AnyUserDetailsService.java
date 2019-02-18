c6d1c63cf27efd96c721a76acbdc9b35bba3c998
邓天骄 <jiao_snow@126.com>
2019-02-18 23:29:10 +0800

    1.添加Spring Security 登陆页面 2.添加数据库实现 3.加入mybatis依赖 明日计划： 1.数据库验证实现完毕 2.集成mybatis plus 3.集成lombok

---
+++ src/main/java/com/love/iLove/config/AnyUserDetailsService.java
@@ -0,0 +1,17 @@
+package com.love.iLove.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AnyUserDetailsService implements UserDetailsService {
+
+//    @Autowired
+//    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
