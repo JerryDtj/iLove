0e669c8eea4b1f00c1dc2f672be80c8bf4f8cca9
邓天骄 <jiao_snow@126.com>
2019-02-18 23:33:45 +0800

    优化导包

--- src/main/java/com/love/iLove/config/AnyUserDetailsService.java
+++ src/main/java/com/love/iLove/config/AnyUserDetailsService.java
@@ -1,6 +1,5 @@
 package com.love.iLove.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
--- src/main/java/com/love/iLove/config/WebSecurityConfig.java
+++ src/main/java/com/love/iLove/config/WebSecurityConfig.java
@@ -1,13 +1,9 @@
 package com.love.iLove.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
