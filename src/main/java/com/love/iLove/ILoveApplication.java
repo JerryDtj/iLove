0c8e2103e6a558db6ce06dd73952c1157e16b7de
d765747369 <765747369@qq.com>
2019-02-19 13:59:59 +0800

    测试https

--- src/main/java/com/love/iLove/ILoveApplication.java
+++ src/main/java/com/love/iLove/ILoveApplication.java
@@ -1,17 +1,11 @@
 package com.love.iLove;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@MapperScan("com.baomidou.mybatisplus.samples.quickstart.mapper")
+@MapperScan("com.love.iLove.mapper")
 public class ILoveApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/love/iLove/mapper/UserMapper.java
+++ src/main/java/com/love/iLove/mapper/UserMapper.java
@@ -2,6 +2,8 @@ package com.love.iLove.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.love.iLove.pojo.User;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface UserMapper extends BaseMapper<User> {
 }
