158d098a510bfa4d2a53d1c5c0fa76558566d42c
d765747369 <765747369@qq.com>
2019-02-19 15:55:41 +0800

    测试https

--- src/test/java/com/love/iLove/ILoveApplicationTests.java
+++ src/test/java/com/love/iLove/ILoveApplicationTests.java
@@ -1,17 +1,12 @@
 package com.love.iLove;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class ILoveApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+//	@Test
+//	public void contextLoads() {
+//	}
 
 }
 
