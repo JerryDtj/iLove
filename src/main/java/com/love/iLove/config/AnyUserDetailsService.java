21be492ff0edb7804ddbc3a488c863b834545043
邓天骄 <jiao_snow@126.com>
2019-02-21 08:04:19 +0800

    优化无用代码

--- src/main/java/com/love/iLove/config/AnyUserDetailsService.java
+++ src/main/java/com/love/iLove/config/AnyUserDetailsService.java
@@ -1,11 +1,17 @@
 package com.love.iLove.config;
 
+import com.love.iLove.pojo.User;
 import com.love.iLove.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AnyUserDetailsService implements UserDetailsService {
@@ -15,6 +21,20 @@ public class AnyUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = userService.get(username);
+        if (user==null){
+            throw new RuntimeException("user is null");
+        }
+        List<SimpleGrantedAuthority> list = this.getCrantedAuthority(user.getRoles());
+        return new org.springframework.security.core.userdetails.User(username,user.getPassword(),list);
+    }
+
+    private List<SimpleGrantedAuthority> getCrantedAuthority(String roles) {
+        List<SimpleGrantedAuthority> list = new ArrayList<>();
+        String[] role = roles.split(",");
+        for (String s:role){
+            list.add(new SimpleGrantedAuthority(s));
+        }
+        return list;
     }
 }
