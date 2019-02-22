d334dff4102f6e4d00175dc7a8118771fc41e7fb
d765747369 <765747369@qq.com>
2019-02-22 18:14:17 +0800

    增加密码加密

--- src/main/java/com/love/iLove/config/WebSecurityConfig.java
+++ src/main/java/com/love/iLove/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -33,6 +34,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(anyUserDetailsService);
+        auth.userDetailsService(anyUserDetailsService)
+            .passwordEncoder(new BCryptPasswordEncoder())
+        ;
     }
 }
