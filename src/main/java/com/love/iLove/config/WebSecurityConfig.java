834de572709f2b119679e4da7c79ca811c1f1a99
邓天骄 <jiao_snow@126.com>
2019-05-13 08:38:29 +0800

    qq登录，作废此类，移到logincontroller，松耦合

--- src/main/java/com/love/iLove/config/WebSecurityConfig.java
+++ src/main/java/com/love/iLove/config/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package com.love.iLove.config;
 
 import com.love.iLove.filter.jwt.JWTAuthenticationFilter;
 import com.love.iLove.filter.qq.QQAuthenticationFilter;
-import com.love.iLove.filter.qq.QQAuthenticationManager;
 import com.love.iLove.handler.LoginSuccessHandler;
 import com.love.iLove.service.impl.AnyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,7 +33,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/","/login").permitAll()
+                .antMatchers("/","/login/**").permitAll()
                 .anyRequest().authenticated()
                 .antMatchers("/user/**").hasRole("USER")
                 .and()
@@ -44,15 +43,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout().logoutUrl("/logout").logoutSuccessUrl("/login");
         // 在 UsernamePasswordAuthenticationFilter 前添加 QQAuthenticationFilter
-        http.addFilterAt(qqAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-            .addFilterBefore(new JWTAuthenticationFilter(authenticationManager()),UsernamePasswordAuthenticationFilter.class)
+//        http.addFilterAt(qqAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+            http.addFilterAt(new JWTAuthenticationFilter(authenticationManager()),UsernamePasswordAuthenticationFilter.class)
         ;
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(anyUserDetailsService)
-            .passwordEncoder(new BCryptPasswordEncoder())
+        .passwordEncoder(new BCryptPasswordEncoder())
         ;
     }
 
@@ -64,8 +63,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         SimpleUrlAuthenticationSuccessHandler successHandler = new SimpleUrlAuthenticationSuccessHandler();
         successHandler.setAlwaysUseDefaultTargetUrl(true);
         successHandler.setDefaultTargetUrl("/user");
-        authenticationFilter.setAuthenticationManager(new QQAuthenticationManager());
-        authenticationFilter.setAuthenticationSuccessHandler(successHandler);
+//        authenticationFilter.setAuthenticationManager(new QQAuthenticationManager());
+//        authenticationFilter.setAuthenticationSuccessHandler(successHandler);
         return authenticationFilter;
     }
 }
