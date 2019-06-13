1253b6aafba523c10ad0bbf6a914261ec0f259d2
邓天骄 <jiao_snow@126.com>
2019-06-13 12:56:46 +0800

    删除无用代码

--- src/main/java/com/love/ilove/utils/MailUtil.java
+++ src/main/java/com/love/ilove/utils/MailUtil.java
@@ -13,7 +13,6 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Properties;
-import java.util.Random;
 
 /**
  * @author Jerry
@@ -36,8 +35,6 @@ public class MailUtil {
     private String isAuth;
     @Value("${emai.usessl}")
     private String usessl;
-//    @Value("${emai.fromUser}")
-//    private String fromUser;
 
 
 
@@ -65,11 +62,8 @@ public class MailUtil {
         for (int i = 0; i < toUser.length; i++) {
             addresses[i] = new InternetAddress(toUser[i]);
         }
-        message.setRecipients(Message.RecipientType.TO, addresses);
         // 设置收件人邮箱地址
-//        message.setRecipients(Message.RecipientType.TO, new InternetAddress[]{new InternetAddress("jiao_snow@126.com"),new InternetAddress("xxx@qq.com"),new InternetAddress("xxx@qq.com")});
-        //一个收件人
-//        message.setRecipient(Message.RecipientType.TO, new InternetAddress("jiao_snow@126.com"));
+        message.setRecipients(Message.RecipientType.TO, addresses);
         // 设置邮件标题
         message.setSubject(title);
         // 设置邮件内容
@@ -86,47 +80,4 @@ public class MailUtil {
 
     }
 
-    public static void main(String[] args) {
-        System.out.println(new Random().nextInt(999999));
-    }
-
-
-   /* public static void main(String[] args) throws  MessagingException {
-        MailUtil.sendTextMail(new String[]{"jiao_snow@126.com"},"content","title");
-
-        Properties properties = new Properties();
-        // 连接协议
-        properties.put("mail.transport.protocol", "smtp");
-        // 主机名
-        properties.put("mail.smtp.host", "smtp.exmail.qq.com");
-        // 端口号
-        properties.put("mail.smtp.port", 465);
-        properties.put("mail.smtp.auth", "true");
-        // 设置是否使用ssl安全连接 ---一般都使用
-        properties.put("mail.smtp.ssl.enable", "true");
-        // 设置是否显示debug信息 true 会在控制台显示相关信息
-        properties.put("mail.debug", "true");
-        // 得到回话对象
-        Session session = Session.getInstance(properties);
-        // 获取邮件对象
-        Message message = new MimeMessage(session);
-        // 设置发件人邮箱地址
-        message.setFrom(new InternetAddress("jerry@mail.tianzijiaozi.top"));
-        // 设置收件人邮箱地址
-//        message.setRecipients(Message.RecipientType.TO, new InternetAddress[]{new InternetAddress("jiao_snow@126.com"),new InternetAddress("xxx@qq.com"),new InternetAddress("xxx@qq.com")});
-        //一个收件人
-        message.setRecipient(Message.RecipientType.TO, new InternetAddress("jiao_snow@126.com"));
-        // 设置邮件标题
-        message.setSubject("xmqtest");
-        // 设置邮件内容
-        message.setText("邮件内容邮件内容邮件内容xmqtest");
-        // 得到邮差对象
-        Transport transport = session.getTransport();
-        // 连接自己的邮箱账户
-        // 密码为QQ邮箱开通的stmp服务后得到的客户端授权码
-        transport.connect("jerry@mail.tianzijiaozi.top", "Ilove1314");
-        // 发送邮件
-        transport.sendMessage(message, message.getAllRecipients());
-        transport.close();
-    }*/
 }
